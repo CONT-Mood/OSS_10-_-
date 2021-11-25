500fe03a6ec398f473292ddac546196882e581c5
Jason <66013853+ahmola@users.noreply.github.com>
2021-11-25 17:39:12 +0900

    main

--- Subway_Convenience.py
+++ Subway_Convenience.py
@@ -6,7 +6,6 @@ from geopy.geocoders import Nominatim
 import requests
 from bs4 import BeautifulSoup
 
-# 지하철역 정보를 사이트에서 받아옴
 response = requests.get('http://www.seoulmetro.co.kr/kr/page.do?menuIdx=366#none')
 soup = BeautifulSoup(response.content, 'html.parser')
 table = soup.find('table', { 'class': 'tbl-type1 t-col'})
@@ -289,7 +288,6 @@ for tr in div.find_all('tr'):
                             voice_inducer
                             ])
             tempnumber = tempnumber + 1
-            
 # 입력받은 역이름 호선을 이용하여 필요한 정보만을 추출함(1~9호선만 존재)
 def find_info(station, station_number):
   if(station_number == 1):
@@ -342,7 +340,7 @@ with open("station_coordinate.xlsx", "wb") as file:
 app = Nominatim(user_agent='tutorial')
 station_number = input("\n몇 호선인가요?(숫자만 입력해주세요.)\n")
 station_number = int(station_number)
-station = input("\n역이름을 입력해주세요(예: 서울역)\n")
+station = input("\n역이름을 입력해주세요(1~4호선은 역이름 뒤에 역을 붙혀주세요. 예: 1호선 서울역 -> 서울역, 6호선 안암역 -> 안암)\n")
 
 station_info = find_info(station, station_number)
 location = app.geocode(station)
@@ -364,14 +362,19 @@ if (station_info is not None):
                     popup= "<pre>"+ss[int(num)-1] +"0개</pre>",
                     icon = folium.Icon(color='blue'))
     marker.add_to(g_map)
+else:
+  marker = folium.Marker([location.point.latitude, location.point.longitude],
+                    popup= "<pre>"+ss[int(num)-1] +"0개</pre>",
+                    icon = folium.Icon(color='blue'))
+  marker.add_to(g_map)
 
 # 주변 역 표시
 for i in range(1, 439):
     start = (location.point.latitude, location.point.longitude)
     goal = (df.loc[i]['lat'], df.loc[i]['lng'])
     dis = haversine(start, goal)
-    # 주변 약 1킬로미터 역들을 보라색으로 표시
-    if(dis <= 1.5) :
+    # 주변 약 1.5킬로미터 내 역들을 보라색으로 표시, 겹치는 경우의 수 제거를 위해 0.5km 이상으로 둠
+    if(dis <= 1.5) & (dis > 0.5) :
       line = int(df.loc[i]['line'][1:2])
       station_info = find_info(df.loc[i]['name'], line)
       if (station_info is not None):
@@ -380,6 +383,4 @@ for i in range(1, 439):
                   popup="<pre>"+ss[int(num)-1] + station_info[int(num)] + "개</pre>",
                   icon = folium.Icon(color='purple'))
           close_marker.add_to(g_map)
-
-# 지도 출력
 g_map
